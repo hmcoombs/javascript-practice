4acee5ad35712a422bd5b5bc23f4cf7e87f3ba4a
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-09-22 18:45:25 -0400

    accept error fix

--- js/script.js
+++ js/script.js
@@ -3,8 +3,4 @@
 to better animate web pages*/
 
 console.log('Mike');
-<<<<<<< HEAD
 console.log('my favorite food is pizza: with pinapple!')
-=======
-console.log('my favorite food is pizza: with pinapple!')
->>>>>>> c79abf018015bd25ebb790316bd443276f8b0f6f
