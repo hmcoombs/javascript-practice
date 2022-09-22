a8ea173ab87cfd60a6aab269ef40faabc8e7c6a9
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-09-22 18:30:12 -0400

    readd code js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//Start of javascript adventure
+/*looking forward to learning to add javascript
+to better animate web pages*/
+
+console.log('Mike');
+console.log('my favorite food is pizza: with pinapple!')
