1b6ff6a611ea025f3d69324ef6a3925ba70a2779
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-09-26 15:56:58 -0400

    day-2 chal entry

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ to better animate web pages*/
 
 console.log('Mike');
 console.log('my favorite food is pizza: with pinapple!');
+
+
+
+let text = "Supercalifragilisticexpialidocious";
+
+// let length = text.length;
+
+// document.getElementById("demo").innerHTML = length;
+
+console.log(2022);
+
+text.length
+
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
+
