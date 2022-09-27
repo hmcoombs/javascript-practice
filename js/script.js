fda98c5df4f81ef91d87c7d61d324ab69faa543b
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-09-27 12:42:53 -0400

    additional day2  chal entry

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ to better animate web pages*/
 console.log('Mike');
 console.log('my favorite food is pizza: with pinapple!');
 
-
+console.log(2022);
 
 let text = "Supercalifragilisticexpialidocious";
 
@@ -13,10 +13,16 @@ let text = "Supercalifragilisticexpialidocious";
 
 // document.getElementById("demo").innerHTML = length;
 
-console.log(2022);
-
 text.length
+console.log(text.length);
+
+typeof text.length
+console.log(typeof text.length);
 
 const bigInt = 1234567890123456789012345678901234567890n;
 console.log(bigInt);
+
+typeof 1234567890123456789012345678901234567890n
+console.log(typeof 1234567890123456789012345678901234567890n);
+
 
