49cd90a9db6278d50bd3eb5f1d83948d4517ec1f
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-09-27 18:53:08 -0400

    log in data

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ to better animate web pages*/
 
 console.log('Mike');
 console.log('my favorite food is pizza: with pinapple!');
+
+let text = 'Supercalifragilisticexpialidocious';
+console.log('length of the text is:', text.length);
+let myInt = 50;
+let myBigInt = 4723914780948723948723498n;
+console.log('my integer:', myInt);
+console.log('my big integer:', myBigInt);
+console.log('type of my integer:', typeof myInt);
+console.log('type of my big int:', typeof myBigInt);
