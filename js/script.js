1d85edfa82f0229c2fdc638a128c56e9c8144265
smackmhard <104869058+smackmhard@users.noreply.github.com>
2022-10-03 21:47:09 -0400

    del old code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,54 @@
-//Start of javascript adventure
-/*looking forward to learning to add javascript
-to better animate web pages*/
+// ------------ DAILY CHALLENGE ------------
+// Prompt the user for two different numbers
+// Convert the values to integers
+// Store the two integers as variables
+// Compare the two numbers
+// Alert the larger number
 
-console.log('Mike');
-console.log('my favorite food is pizza: with pinapple!');
+
+let firstNumber = prompt("Please provide the first number", "0");
+
+let secondNumber = prompt("Please provide the second number", "0");
+
+
+
+// Convert the values to integers
+// Store the two integers as variables
+let intFN = Number(firstNumber);
+let intSN = Number(secondNumber);
+console.log(intFN)
+console.log(secondNumber)
+
+while(intFN === NaN) {
+    prompt("Please input only a first number")
+{
+    if (intFN !== NaN) {
+        break;
+}
+}
+
+while(intSN === NaN) {
+    prompt("Please input only a second number")
+}
+
+
+
+// Compare the two numbers
+// Alert the larger number
+if (intFN === intSN) {
+    alert(`The numbers are equal.: ${intFN} - ${intSN}`);
+} else if (intFN > intSN) {
+    alert(`The first number is larger ${intFN}`);
+} else {
+ alert(`The second number is larger ${intSN}`);
+}
+
+
+
+
+
+
+// ------------ TAKE HOME CHALLENGE ------------
+// Add an edge case to your daily challenge code
+// Check if the user input anything other than a number
+// If so, prompt the user again to input a number
